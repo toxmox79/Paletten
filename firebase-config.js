eccc3fbd9449ee53890472db282992299d929fb9
toxmox79 <tomlux@gmx.de>
2026-05-07 13:37:57 +0200

    Add Firebase config for hosting

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDQQtHFTFz2aNpZg_s3Sfgxe95TGPNQWtI",
+  authDomain: "palettenverwaltung-717b1.firebaseapp.com",
+  projectId: "palettenverwaltung-717b1",
+  storageBucket: "palettenverwaltung-717b1.firebasestorage.app",
+  messagingSenderId: "362007316203",
+  appId: "1:362007316203:web:fe21424890a7d550ed20ae"
+};
